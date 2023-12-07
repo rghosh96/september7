ef2831b20d6fc9b8a6b4a5dfe3cffc119dc2bb61
rashi <rashighosh96@gmail.com>
2023-12-07 10:34:47 -0500

    updated background

--- src/JS/App.js
+++ src/JS/App.js
@@ -1,33 +1,43 @@
 import '../index.css'
 import '../CSS/App.css';
-import ThemeToggle from './ThemeToggle';
 import { ReactComponent as Logo } from '../assets/Logo.svg'
 
 function App() {
   return (
     <div className="App">
-      <ThemeToggle/>
-
-      <div className="navigation">
-        <p className="nav-item">our story</p>
-        <p className="nav-item">gallery</p>
-        <Logo className="logo"/>
-        <p className="nav-item">information</p>
-        <p className="nav-item">registry</p>
+      <div className="bg-image">
+      
+      
       </div>
+      
+      <div className="navigation">
+        <div className="nav-column">
+          <p className="nav-item"><a href='/'>our story</a></p>
+          <p className="nav-item"><a href='/'>gallery</a></p>
+        </div>
+
+        <div class="logo-column">
+          <Logo className="logo"/>
+        </div>
+          
+        <div className="nav-column">
+          <p className="nav-item"><a href='/'>information</a></p>
+          <p className="nav-item"><a href='/'>registry</a></p>
+        </div>
+      </div>
+
 
       <div className="title-area">
         <h1 className="title">Rashi & Gabriel</h1>
         <hr/>
         <h3 className="subtitle">
-            September 7, 2024
+            7th september 2024
           </h3>
       </div>
       
       
       
       <div className="subtitle">
-          
       </div>
      
     </div>
